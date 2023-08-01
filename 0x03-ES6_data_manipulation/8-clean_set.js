1893cd736d9535598113eecf5ea2d263385e3daf
Ifedolapoomoniyi <ifedolapo0001@gmail.com>
2023-08-01 04:27:42 +0100

    returns all values that start with the given string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
+  if (!startString || typeof startString !== 'string' || startString.length === 0) {
     return '';
   }
   const result = [];
